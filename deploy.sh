5a643f7710d764a14a86f75e5c84879f758595da
RyhorPaprauko <ryhorpaprauka@mail.com>
2019-12-31 12:01:48 +0300

    some fix in sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-docker build -t grinman/multi-visit:latest -t grinman/multi-visit:$SHA -f . .
+docker build -t grinman/multi-visit:$SHA -f . .
 echo  "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
-docker push grinman/multi-visit:latest
 docker push grinman/multi-visit:$SHA
 
 kubectl apply -f k8s
