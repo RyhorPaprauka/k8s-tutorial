26e7d43aadc322e346674a47305339d8d57dbb9c
RyhorPaprauko <ryhorpaprauka@mail.com>
2019-12-31 13:55:49 +0300

    nn

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
-docker build -t grinman/multi-visit:$SHA -f .
-echo  "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
-docker push grinman/multi-visit:$SHA
-
 kubectl apply -f k8s
 kubectl set image deployments/visit-deployment visit=grinman/multi-visit:$SHA
